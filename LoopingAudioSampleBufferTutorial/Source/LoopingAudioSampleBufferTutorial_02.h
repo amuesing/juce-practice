586a08e4150d46380ab4d7cfd18917cec8f36189
muesinga <contact@aaronmuesing.com>
2021-12-08 08:25:58 -0800

    looping

---
+++ LoopingAudioSampleBufferTutorial/Source/LoopingAudioSampleBufferTutorial_02.h
@@ -0,0 +1,194 @@
+/*
+  ==============================================================================
+
+   This file is part of the JUCE tutorials.
+   Copyright (c) 2020 - Raw Material Software Limited
+
+   The code included in this file is provided under the terms of the ISC license
+   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
+   To use, copy, modify, and/or distribute this software for any purpose with or
+   without fee is hereby granted provided that the above copyright notice and
+   this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES,
+   WHETHER EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR
+   PURPOSE, ARE DISCLAIMED.
+
+  ==============================================================================
+*/
+
+/*******************************************************************************
+ The block below describes the properties of this PIP. A PIP is a short snippet
+ of code that can be read by the Projucer and used to generate a JUCE project.
+
+ BEGIN_JUCE_PIP_METADATA
+
+ name:             LoopingAudioSampleBufferTutorial
+ version:          2.0.0
+ vendor:           JUCE
+ website:          http://juce.com
+ description:      Explores audio sample buffer looping.
+
+ dependencies:     juce_audio_basics, juce_audio_devices, juce_audio_formats,
+                   juce_audio_processors, juce_audio_utils, juce_core,
+                   juce_data_structures, juce_events, juce_graphics,
+                   juce_gui_basics, juce_gui_extra
+ exporters:        xcode_mac, vs2019, linux_make
+
+ type:             Component
+ mainClass:        MainContentComponent
+
+ useLocalCopy:     1
+
+ END_JUCE_PIP_METADATA
+
+*******************************************************************************/
+
+
+#pragma once
+
+//==============================================================================
+class MainContentComponent   : public juce::AudioAppComponent
+{
+public:
+    MainContentComponent()
+    {
+        addAndMakeVisible (openButton);
+        openButton.setButtonText ("Open...");
+        openButton.onClick = [this] { openButtonClicked(); };
+
+        addAndMakeVisible (clearButton);
+        clearButton.setButtonText ("Clear");
+        clearButton.onClick = [this] { clearButtonClicked(); };
+
+        addAndMakeVisible (levelSlider);
+        levelSlider.setRange (0.0, 1.0);
+        levelSlider.onValueChange = [this] { currentLevel = (float) levelSlider.getValue(); };
+
+        setSize (300, 200);
+
+        formatManager.registerBasicFormats();
+    }
+
+    ~MainContentComponent() override
+    {
+        shutdownAudio();
+    }
+
+    void prepareToPlay (int, double) override {}
+
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
+    {
+        auto level = currentLevel;
+        auto startLevel = level == previousLevel ? level : previousLevel;
+
+        auto numInputChannels = fileBuffer.getNumChannels();
+        auto numOutputChannels = bufferToFill.buffer->getNumChannels();
+
+        auto outputSamplesRemaining = bufferToFill.numSamples;
+        auto outputSamplesOffset = bufferToFill.startSample;
+
+        while (outputSamplesRemaining > 0)
+        {
+            auto bufferSamplesRemaining = fileBuffer.getNumSamples() - position;
+            auto samplesThisTime = juce::jmin (outputSamplesRemaining, bufferSamplesRemaining);
+
+            for (auto channel = 0; channel < numOutputChannels; ++channel)
+            {
+                bufferToFill.buffer->copyFrom (channel,
+                                               outputSamplesOffset,
+                                               fileBuffer,
+                                               channel % numInputChannels,
+                                               position,
+                                               samplesThisTime);
+
+                bufferToFill.buffer->applyGainRamp (channel, outputSamplesOffset, samplesThisTime, startLevel, level);
+            }
+
+            outputSamplesRemaining -= samplesThisTime;
+            outputSamplesOffset += samplesThisTime;
+            position += samplesThisTime;
+
+            if (position == fileBuffer.getNumSamples())
+                position = 0;
+        }
+
+        previousLevel = level;
+    }
+
+    void releaseResources() override
+    {
+        fileBuffer.setSize (0, 0);
+    }
+
+    void resized() override
+    {
+        openButton .setBounds (10, 10, getWidth() - 20, 20);
+        clearButton.setBounds (10, 40, getWidth() - 20, 20);
+        levelSlider.setBounds (10, 70, getWidth() - 20, 20);
+    }
+
+private:
+    void openButtonClicked()
+    {
+        shutdownAudio();                                                                            // [1]
+
+        chooser = std::make_unique<juce::FileChooser> ("Select a Wave file shorter than 2 seconds to play...",
+                                                       juce::File{},
+                                                       "*.wav");
+        auto chooserFlags = juce::FileBrowserComponent::openMode
+                          | juce::FileBrowserComponent::canSelectFiles;
+
+        chooser->launchAsync (chooserFlags, [this] (const juce::FileChooser& fc)
+        {
+            auto file = fc.getResult();
+
+            if (file == juce::File{})
+                return;
+
+            std::unique_ptr<juce::AudioFormatReader> reader (formatManager.createReaderFor (file)); // [2]
+
+            if (reader.get() != nullptr)
+            {
+                auto duration = (float) reader->lengthInSamples / reader->sampleRate;               // [3]
+
+                if (duration < 2)
+                {
+                    fileBuffer.setSize ((int) reader->numChannels, (int) reader->lengthInSamples);  // [4]
+                    reader->read (&fileBuffer,                                                      // [5]
+                                  0,                                                                //  [5.1]
+                                  (int) reader->lengthInSamples,                                    //  [5.2]
+                                  0,                                                                //  [5.3]
+                                  true,                                                             //  [5.4]
+                                  true);                                                            //  [5.5]
+                    position = 0;                                                                   // [6]
+                    setAudioChannels (0, (int) reader->numChannels);                                // [7]
+                }
+                else
+                {
+                    // handle the error that the file is 2 seconds or longer..
+                }
+            }
+        });
+    }
+
+    void clearButtonClicked()
+    {
+        shutdownAudio();
+    }
+
+    //==========================================================================
+    juce::TextButton openButton;
+    juce::TextButton clearButton;
+    juce::Slider levelSlider;
+
+    std::unique_ptr<juce::FileChooser> chooser;
+
+    juce::AudioFormatManager formatManager;
+    juce::AudioSampleBuffer fileBuffer;
+    int position = 0;
+
+    float currentLevel = 0.0f, previousLevel = 0.0f;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
